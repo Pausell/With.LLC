12cf6403af2c00056da9231bbfb1e343b00138b1
ll <65037958+Pausell@users.noreply.github.com>
2023-09-17 02:18:28 -0700

    2

--- _global/head.php
+++ _global/head.php
@@ -11,32 +11,29 @@ $subdir_count = substr_count($current_path, '/') - 1;
 // Construct the relative path from the root directory to the current page
 $path = str_repeat('../', $subdir_count);
 
-// Resources Directory
-$add = "add/";
-
 // Build the canonical URL using the protocol, hostname, and current path
 $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
 $hostname = $_SERVER['HTTP_HOST'];
 $canonical = $protocol . "://" . $hostname . $current_path;
 $canonical = str_replace(".php", "", $canonical);
 
-$style = 'style.css';
+$style = '/style.css';
 ?>
-<head>
- <title><?php echo $title; ?></title>
- <meta name="description" content="<?php echo $description; ?>">
- <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
- <link rel="shortcut icon" href="favicon.ico?n=2023">
-  <link rel="shortcut icon" type="image/png" href="favicon.png?n=2023">
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon32.png?n=2023">
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon16.png?n=2023">
-  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-  <link rel="manifest" href="/site.webmanifest?n=2023">
-  <link rel="mask-icon" href="safari-pinned-tab.svg?n=2023" color="#ff0000">
-  <meta name="msapplication-TileColor" content="#00a300">
-  <meta name="theme-color" content="#ffffff">
- <link rel="canonical" href="<?php echo $canonical; ?>"/>
- <link rel="stylesheet" href="<?php echo $index . $path . $style; ?>">
-<?php echo $internal_style ?>
-<?php echo $internal_head ?>
-</head>
+ <head>
+  <title><?php echo $title; ?></title>
+  <meta name="description" content="<?php echo $description; ?>">
+  <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+  <link rel="shortcut icon" href="favicon.ico?n=2023">
+   <link rel="shortcut icon" type="image/png" href="favicon.png?n=2023">
+   <link rel="icon" type="image/png" sizes="32x32" href="favicon32.png?n=2023">
+   <link rel="icon" type="image/png" sizes="16x16" href="favicon16.png?n=2023">
+   <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+   <link rel="manifest" href="/site.webmanifest?n=2023">
+   <link rel="mask-icon" href="safari-pinned-tab.svg?n=2023" color="#ff0000">
+   <meta name="msapplication-TileColor" content="#00a300">
+   <meta name="theme-color" content="#ffffff">
+  <link rel="canonical" href="<?php echo $canonical; ?>"/>
+  <link rel="stylesheet" href="<?php echo $index . $path . $style; ?>">
+ <?php echo $internal_style ?>
+ <?php echo $internal_head ?>
+ </head>
--- index.php
+++ index.php
@@ -1,26 +1,11 @@
 <?php
 $index = '';
 $path = '_global';
+$internal_style = '<!--_module-->
+  <link rel-"stylesheet" href="_module/search.css">';
 ?>
 <html>
- <head>
-  <title>With.LLC</title>
-  <meta name="description" content="Written Is The Holy Diadem Of Time's Legible Language Cartographer">
-  <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-  <link rel="shortcut icon" href="favicon.ico?n=2023">
-   <link rel="shortcut icon" type="image/png" href="favicon.png?n=2023">
-   <link rel="icon" type="image/png" sizes="32x32" href="favicon32.png?n=2023">
-   <link rel="icon" type="image/png" sizes="16x16" href="favicon16.png?n=2023">
-   <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-   <link rel="manifest" href="/site.webmanifest?n=2023">
-   <link rel="mask-icon" href="safari-pinned-tab.svg?n=2023" color="#ff0000">
-   <meta name="msapplication-TileColor" content="#00a300">
-   <meta name="theme-color" content="#ffffff">
-  <link rel="canonical" href="https://with.llc"/>
-  <link rel="stylesheet" href="<?php echo $index . $path . ('/style.css'); ?>">
-  <!--_module-->
-  <link rel-"stylesheet" href="_module/search.css">
- </head>
+ <?php include $index . $path . ('head.php'); ?>
  <body>
   <div id="container">
    <span style="position:absolute;opacity:.1;z-index:-1"><h1 style="margin:0">Written Is The Holy <em style="opacity:.8">Diadem Of Time&#39;s </em> Legible Language Cartographer</h1></span>
