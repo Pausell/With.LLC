80bfc7fde2f65dcd18ad66755aa671698aadf794
ll <65037958+Pausell@users.noreply.github.com>
2023-09-17 03:45:38 -0700

    3

--- bereishith/index.php
+++ bereishith/index.php
@@ -2,13 +2,13 @@
 $title = 'Bereishith';
 $description = 'In beginning...';
 $index = '../';
-$path = '_global';
+$path = '_global/';
 ?>
 <html>
- <?php include $index . $path . ('/head.php'); ?>
+ <?php include $index . $path . ('head.php'); ?>
  <body>
   <div id="container">
-   <?php include $index . $path . ('/navigation.php'); ?>
+   <?php include $index . $path . ('navigation.php'); ?>
    <div style="position:relative">
    <span style="position:absolute;opacity:.1;z-index:-1"><h1 style="margin:0">Alpha's Lair Triumphing <em style="opacity:.8">Authority</em> Reoccuringly</h1></span>
    </div>
