20baeaf4e65ef088f95fdc135d469080fe05e192
ll <65037958+Pausell@users.noreply.github.com>
2023-09-17 01:07:30 -0700

    09-17-2023

--- index.php
+++ index.php
@@ -24,80 +24,17 @@ $path = '_global';
  <body>
   <?php include '_global/navigation.php'; ?>
   <span style="position:absolute;opacity:.1;z-index:-1"><h1 style="margin:0">Written Is The Holy <em style="opacity:.8">Diadem Of Time&#39;s </em> Legible Language Cartographer</h1></span>
+  
   <form id="search-form">
-   <h1><input type="text" id="search-input" placeholder="W/" />
+   <h1><input type="search" id="search-input" placeholder="W/" />
     <button type="submit" id="search-button">Search</button></h1>
   </form>
-  <div id="search-suggestions">
-   <!--<div class="suggestion">Suggestion 1</div>
+  <!--<div id="search-suggestions">
+   <div class="suggestion">Suggestion 1</div>
    <div class="suggestion">Suggestion 2</div>
    <div class="suggestion">Suggestion 3</div>
-   <div class="suggestion">Suggestion 4</div>-->
-  </div>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script>
-  $(document).ready(function() {
-  // Function to fetch and display search suggestions
-  function fetchSuggestions(query) {
-  $.ajax({
-  url: 'fetch_suggestions.php', // Replace with your PHP script to fetch suggestions
-  type: 'GET',
-  data: { query: query },
-  success: function(response) {
-  $('#search-suggestions').html(response);
-  $('#search-suggestions').show();
-  }
-  });
-  }
-
-  // Event handler for form submission
-  $('#search-form').submit(function(e) {
-  e.preventDefault();
-  var query = $('#search-input').val();
-  fetchSuggestions(query);
-  });
-
-  // Event handler for input change
-  $('#search-input').on('input', function() {
-  var query = $(this).val();
-  fetchSuggestions(query);
-  });
-
-  // Event handler for suggestion selection
-  $(document).on('click', '.suggestion', function() {
-  var suggestion = $(this).text();
-  $('#search-input').val(suggestion);
-  $('#search-suggestions').hide();
-  });
-  });
-  </script>
-  <script>
-  $(document).ready(function() {
-    // Function to update search button text and behavior
-    function updateSearchButton() {
-        var query = $('#search-input').val().trim();
-        var button = $('#search-button');
-
-        if (query === '') {
-            // When input is empty or just whitespace
-            button.text('In');
-            button.attr('href', 'in.php');
-        } else {
-            // When input has content
-            button.text('Search');
-            button.removeAttr('href');
-        }
-    }
-
-    // Event handler for input change
-    $('#search-input').on('input', function() {
-        updateSearchButton();
-    });
-
-    // Initial setup
-    updateSearchButton();
-  });
-  </script>
+   <div class="suggestion">Suggestion 4</div>
+  </div>-->
   <?php if (!empty($error_message)): ?>
    <p class="error-message"><?= $error_message ?></p>
   <?php endif; ?>
