6bb6f16eac04f78c375288182e1a8d041f3227a7
ll <65037958+Pausell@users.noreply.github.com>
2023-09-26 02:59:31 -0700

    09-26-2023

--- bible/testamentfoot.php
+++ bible/testamentfoot.php
@@ -1,5 +1,5 @@
 <script>
-//Hide/Display According to Selection
+// Hide/Display According to Selection
 
 // Step 1: Hide unchecked li with input type="checkbox" on page load
 const liSections = document.querySelectorAll('li section');
@@ -16,38 +16,12 @@ liSections.forEach((liSection) => {
 // Step 2: Add "Table of Contents" anchor to .igation to toggle visibility of initially hidden li
 const tableOfContents = document.createElement('a');
 tableOfContents.href = '#';
-tableOfContents.innerHTML = `<svg width="100%" class="white" width="260" height="261" viewBox="0 0 260 261" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0_702_524)">
-<rect width="260" height="260" transform="translate(0 1.55469)" fill="#13003C"/>
-<rect width="260" height="260" transform="translate(0 1.55469)" fill="url(#paint0_linear_702_524)" fill-opacity="0.2"/>
-<path fill-rule="evenodd" clip-rule="evenodd" d="M180 57H80V107H30V207H34.1279V111.128H80V157H84.1279V111.128H125.871V207H130H134.128V111.128H175.871V157H180V111.128H225.871V207H230V107H180V57ZM84.1279 107H130H175.871V61.1284H84.1279V107Z" fill="#FF0000"/>
-</g>
-<defs>
-<linearGradient id="paint0_linear_702_524" x1="130" y1="0" x2="130" y2="260" gradientUnits="userSpaceOnUse">
-<stop stop-color="white" stop-opacity="0.75"/>
-<stop offset="1" stop-color="white" stop-opacity="0"/>
-</linearGradient>
-<clipPath id="clip0_702_524">
-<rect y="0.710938" width="260" height="260" rx="57.54" fill="white"/>
-</clipPath>
-</defs>
-</svg>
-<svg width="100%" class="black" width="260" height="261" viewBox="0 0 260 261" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0_702_524)">
-<rect width="260" height="260" transform="translate(0 1.55469)" fill="#13003C"/>
-<rect width="260" height="260" transform="translate(0 1.55469)" fill="url(#paint0_linear_702_524)" fill-opacity="0.2"/>
-<path fill-rule="evenodd" clip-rule="evenodd" d="M180 57H80V107H30V207H34.1279V111.128H80V157H84.1279V111.128H125.871V207H130H134.128V111.128H175.871V157H180V111.128H225.871V207H230V107H180V57ZM84.1279 107H130H175.871V61.1284H84.1279V107Z" fill="#FF0000"/>
-</g>
-<defs>
-<linearGradient id="paint0_linear_702_524" x1="130" y1="0" x2="130" y2="260" gradientUnits="userSpaceOnUse">
-<stop stop-color="white" stop-opacity="0.75"/>
-<stop offset="1" stop-color="white" stop-opacity="0"/>
-</linearGradient>
-<clipPath id="clip0_702_524">
-<rect y="0.710938" width="260" height="260" rx="57.54" fill="white"/>
-</clipPath>
-</defs>
-</svg>`;
+tableOfContents.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <path fill="none" d="M0 0h24v24H0z"/>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"/>
+  </svg>
+`;
 
 tableOfContents.addEventListener('click', () => {
   liSections.forEach((liSection) => {
@@ -57,6 +31,9 @@ tableOfContents.addEventListener('click', () => {
       li.style.display = li.style.display === 'none' ? '' : 'none';
     }
   });
+
+  // Toggle the active class for the "Table of Contents" anchor
+  tableOfContents.classList.toggle('active');
 });
 
 document.querySelector('.igation').appendChild(tableOfContents);
