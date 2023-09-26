0266d159f803698a88cda1fd83b1928499beef25
ll <65037958+Pausell@users.noreply.github.com>
2023-09-26 02:02:11 -0700

    09-26-2023

--- _global/head.php
+++ _global/head.php
@@ -7,9 +7,9 @@ $style = 'style.css';
   <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
   <link rel="shortcut icon" href="<?php echo $index; ?>favicon.ico?n=2023">
    <link rel="shortcut icon" type="image/png" href="<?php echo $index; ?>favicon.png?n=2023">
-   <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $index . ?>favicon32.png?n=2023">
-   <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $index . ?>favicon16.png?n=2023">
-   <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $index . ?>apple-touch-icon.png">
+   <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $index; ?>favicon32.png?n=2023">
+   <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $index; ?>favicon16.png?n=2023">
+   <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $index; ?>apple-touch-icon.png">
    <link rel="manifest" href="/site.webmanifest?n=2023">
    <link rel="mask-icon" href="safari-pinned-tab.svg?n=2023" color="#ff0000">
    <meta name="msapplication-TileColor" content="#00a300">
