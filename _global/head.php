0ac04a36009fadd436cb2e130f7d6b3e24f72c3e
ll <65037958+Pausell@users.noreply.github.com>
2023-09-26 02:00:46 -0700

    09-26-2023

--- _global/head.php
+++ _global/head.php
@@ -6,7 +6,7 @@ $style = 'style.css';
   <meta name="description" content="<?php echo $description; ?>">
   <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
   <link rel="shortcut icon" href="<?php echo $index; ?>favicon.ico?n=2023">
-   <link rel="shortcut icon" type="image/png" href="<?php echo $index . ?>favicon.png?n=2023">
+   <link rel="shortcut icon" type="image/png" href="<?php echo $index; ?>favicon.png?n=2023">
    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $index . ?>favicon32.png?n=2023">
    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $index . ?>favicon16.png?n=2023">
    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $index . ?>apple-touch-icon.png">
